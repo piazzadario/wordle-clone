e99fb54c68c69bee3ecee8288d25427195c34569
Dario Piazza <dariopiazza007@gmail.com>
2023-10-16 17:37:45 +0200

    chore: letter tile style

--- src/components/LetterTile.js
+++ src/components/LetterTile.js
@@ -1,12 +1,39 @@
 import React from "react";
 import {Card} from "react-bootstrap";
 
+const Status = {
+  correct: 'correct',
+  wrongPosition: 'wrong-position',
+  notPresent: 'not-present',
+}
 
-const LetterTile = ({letter}) => {
+const LetterTile = ({letter, status}) => {
   
+    function letterStyle(){
+      var baseStyle = "letter-tile";
+      if(letter===''){
+        return baseStyle;
+      }
+
+      switch(status){
+        case Status.correct:
+            baseStyle+=' correct';
+          break;
+        case Status.wrongPosition:
+            baseStyle+=' wrong-position';
+            break;
+        case Status.notPresent:
+            baseStyle+=' not-present';
+            break;
+        default:
+          break;
+      }
+
+      return baseStyle;
+    }
   
     return (
-      <Card className="letter-tile">
+      <Card className={letterStyle()}>
         <h2>
         {letter.toUpperCase()}
         </h2>
