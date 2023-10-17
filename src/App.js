07efcdd23b684c56dbdfdb0f2ad5e7dde2cc5c46
Dario Piazza <dariopiazza007@gmail.com>
2023-10-17 12:17:19 +0200

    feat: switch theme

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import GameRecapDialog from "./components/GameRecapDialog";
 import AppHeader from "./components/AppHeader";
 import GameHistoryDialog from "./components/GameHistoryDialog";
 
+const getTheme = () => {
+  const theme = localStorage.getItem("theme");
+  if (!theme) {
+    localStorage.setItem("theme", "dark");
+    return "dark";
+  } else {
+    return theme;
+  }
+};
+
+
 
 function App(){
+    const [theme,setTheme] = useState(getTheme())
     const [isGameOver,setIsGameOver] = useState(false);
     const [showRecapDialog,setShowRecapDialog] = useState(false);
     const [lastGameResult,setLastGameResult] = useState(0);
@@ -43,9 +55,23 @@ function App(){
       setShowGameHistory(false);
     }
 
+    function toggleTheme(){
+      var newTheme;
+      if(theme==='dark'){
+        newTheme = 'light';
+      }else{
+        newTheme = 'dark';
+      }
+      setTheme(newTheme);
+      saveTheme(newTheme);
+    }
+
+    function saveTheme(theme){
+      localStorage.setItem("theme", theme);
+    }
     return (
-      <div>
-        <AppHeader onShowGamesHistory={showGameHistoryDialog}/>
+        <div className={theme}>
+        <AppHeader onShowGamesHistory={showGameHistoryDialog} onToggleTheme={toggleTheme} theme={theme}/>
         <Game onGameOver={onGameOver} isGameOver={isGameOver} onNewGame={onNewGame}/>
         <GameRecapDialog show={showRecapDialog} onHide={hideRecapDialog} lastGameResult={lastGameResult} onPlayAgain={startNewGame}/>
         <GameHistoryDialog show={showGameHistory} onHide={hideGameHistoryDialog}/>
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,16 +1,22 @@
-import { Button, Container, Navbar } from "react-bootstrap";
+import { Button, Container, Form, Navbar } from "react-bootstrap";
 import "react-simple-keyboard/build/css/index.css";
 
 
-function AppHeader ({onShowGamesHistory}) {
+function AppHeader ({onShowGamesHistory, onToggleTheme, theme}) {
 
   return (
     <Navbar className='app-header'>
       <Container>
           <Navbar.Brand className="brand">Wordle</Navbar.Brand>
-          <Button onClick={onShowGamesHistory}>
+          
+          <Form className="d-flex">
+            <Form.Check checked={theme==='dark'} type="switch" label='Dark mode' className="theme-switcher me-4" onClick={onToggleTheme}>
+            </Form.Check>
+
+            <Button onClick={onShowGamesHistory}>
               Game history
           </Button>
+          </Form>
         </Container>
     </Navbar>
   );
